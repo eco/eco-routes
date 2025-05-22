f6a457407f9c1eb535247f88190a023c261fe18b
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-22 10:41:44 -0400

    setting the hardhat compiler version to match the foundry version of 0.8.27 (#251)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,7 +14,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.26',
+        version: '0.8.27',
         settings: {
           viaIR: true,
           optimizer: {
