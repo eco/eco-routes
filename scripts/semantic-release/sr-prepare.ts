03485e8b6ec605d4ade21c45a63c670bdc36765e
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-28 16:57:01 -0400

    fix: move versioning into the preprack step

--- scripts/semantic-release/sr-prepare.ts
+++ scripts/semantic-release/sr-prepare.ts
@@ -33,6 +33,7 @@ import { promisify } from 'util'
 import { exec } from 'child_process'
 import { verifyContracts } from './verify-contracts'
 import { deploySingletonFactory } from './sr-singleton-factory'
+import { version as versionPackage} from './sr-version'
 
 dotenv.config()
 
@@ -81,6 +82,8 @@ export async function prepare(
   pluginConfig: SemanticPluginConfig,
   context: SemanticContext,
 ): Promise<void> {
+  await versionPackage(pluginConfig, context) // Ensure version is set before prepare
+  
   const { nextRelease, logger, cwd } = context
 
   if (!nextRelease) {
