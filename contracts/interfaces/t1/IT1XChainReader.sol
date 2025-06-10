4e312213649ef80fc86507b958f3644b4e121090
Angelopoulos <87050409+christianangelopoulos@users.noreply.github.com>
2025-06-10 16:36:44 -0500

    almost working

---
+++ contracts/interfaces/t1/IT1XChainReader.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+interface IT1XChainReader {
+    struct ReadRequest {
+        uint32 destinationDomain;
+        address targetContract;
+        uint256 gasLimit;
+        uint64 minBlock;
+        bytes callData;
+    }
+
+    error InvalidBatchIndex();
+    error InvalidProof();
+    error OnlyProver();
+    error ZeroAddress();
+
+    event Initialized(uint8 version);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event ProofOfReadRootCommitted(uint256 batchIndex);
+    event ReadRequested(
+        bytes32 indexed requestId,
+        uint32 indexed destinationDomain,
+        address targetContract,
+        address requester,
+        uint256 gasLimit,
+        uint64 minBlock,
+        bytes callData,
+        uint256 nonce
+    );
+
+    function MESSENGER() external view returns (address);
+    function commitProofOfReadRoot(uint256 batchIndex, bytes32 newRoot) external payable;
+    function nextBatchIndex() external view returns (uint256);
+    function nonce() external view returns (uint256);
+    function owner() external view returns (address);
+    function proofOfReadRoots(uint256 batchIndex) external view returns (bytes32 root);
+    function prover() external view returns (address);
+    function renounceOwnership() external;
+    function requestRead(ReadRequest memory request) external payable returns (bytes32 requestId);
+    function transferOwnership(address newOwner) external;
+    function verifyProofOfRead(bytes memory encodedProofOfRead) external view returns (bytes32, bytes memory);
+}
